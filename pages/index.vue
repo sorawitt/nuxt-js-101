5aa5b1b8d952b0adb28cf3e1bdc870a9b4ba322a
Sorawit Trutsat <sorawit@central.tech>
2020-05-06 15:13:57 +0700

    ep6 get set computed

--- pages/index.vue
+++ pages/index.vue
@@ -9,10 +9,22 @@
 export default {
   data() {
     return {
-      message: 'hello world'
+      message: 'hello world',
+      firstName: 'Sorawit',
+      lastName: 'Trutsat'
     }
   },
   computed: {
+    fullName() {
+      get() {
+        return this.firstName + ' ' + this.lastName
+      }
+
+      set(val) {
+        this.firstName = val.split(' ')[0]
+        this.lastName = val.split(' ')[1]
+      }
+    }
     reversedMessage() {
       return this.message
         .split('')
